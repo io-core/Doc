17af9e5a03fc4f81e955cbfec134df4d7c3d4693
Charles Perkins <chuck@kuracali.com>
2021-02-17 07:50:03 -0800

    regenerated odoc

--- genhtml.sh
+++ genhtml.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function brrt {
+
+echo "grar"
+
+}
+
 function cronch {
 local fnf="$1"
 local fns="${fnf:0:-4}"
@@ -9,17 +15,23 @@ for i in `awk '/toctree/{f=1;}f' $fns.tmp | tail -n +6 | awk '/^$/{exit};$1' | t
 	local src=`echo $i | tr '&' ' ' | awk '{print $1;}'`
 	local dst=`echo $i | tr '&' ' ' | awk '{print $2;}'`
 	echo  "for $fnf from $src to $dst "
-	if [ -f "$src.Mod" ] ; then
-		echo "  in $fnf generating ${dst:1:-1}"
-		awk '/end-documentation-node/{p=0};p;/begin-documentation-node/{p=1}' "$src.Mod" > "${dst:1:-1}"
-		local t=`grep toctree "${dst:1:-1}" | wc | awk '{print $1;}'`
-		echo $t
-		if [ $t != 0 ] ; then
-			cronch "${dst:1:-1}"
+	if [ "${dst:0:1}" == "[" ] ; then
+		if [ -f "$src.Pkg" ] ; then
+			brrt "$src.Pkg" "${dst:1:-1}"
+		fi
+	elif [ "${dst:0:1}" == "<" ] ; then
+		if [ -f "$src.Mod" ] ; then
+			#echo "  in $fnf generating ${dst:1:-1}"
+			awk '/end-documentation-node/{p=0};p;/begin-documentation-node/{p=1}' "$src.Mod" > "${dst:1:-1}"
+			local t=`grep toctree "${dst:1:-1}" | wc | awk '{print $1;}'`
+			#echo $t
+			if [ $t != 0 ] ; then
+				cronch "${dst:1:-1}"
+			fi
 		fi
 	fi
 done
-echo "finalizing $fns.tmp to $fnf"
+#echo "finalizing $fns.tmp to $fnf"
 cat $fns.tmp | sed -e 's/ *\(.*\)<\(.*\).rst>/   \2/g' > $fnf
 
 }
